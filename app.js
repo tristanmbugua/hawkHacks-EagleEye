066bb07611e8e1adffcf9925a25503346c865a1d
Tristan Mbugua <tristanmbugua@gmail.com>
2024-05-18 15:48:02 -0400

    Restart.
    We are beginning working on the front-end now.

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import { KeyPair, keyStores } from "near-api-js";
-// import near from "near-sdk-js";
+// import { KeyPair, keyStores } from "near-api-js";
+// // import near from "near-sdk-js";
 
-const keyStore = new keyStores.InMemoryKeyStore();
-const PRIVATE_KEY = ""
+// const keyStore = new keyStores.InMemoryKeyStore();
+// const PRIVATE_KEY = ""
 
-const config = { 
-  keyStore, // instance of BrowserLocalStorageKeyStore
-  networkId: 'testnet',
-  nodeUrl: 'https://rpc.testnet.near.org',
-  walletUrl: 'https://wallet.testnet.near.org',
-  helperUrl: 'https://helper.testnet.near.org',
-  explorerUrl: 'https://explorer.testnet.near.org'
-};
+// const config = { 
+//   keyStore, // instance of BrowserLocalStorageKeyStore
+//   networkId: 'testnet',
+//   nodeUrl: 'https://rpc.testnet.near.org',
+//   walletUrl: 'https://wallet.testnet.near.org',
+//   helperUrl: 'https://helper.testnet.near.org',
+//   explorerUrl: 'https://explorer.testnet.near.org'
+// };
 
-// inside an async function
-const near_ = await connect(config)
+// // inside an async function
+// const near_ = await connect(config)
 
-console.log(working);
+// console.log(working);
